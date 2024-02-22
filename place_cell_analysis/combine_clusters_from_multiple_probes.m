711dbe45940cce603202ca077fd7ef592f76e09e
masahirotaki <masahirotakigaw@gmail.com>
2024-02-22 20:07:30 +0000

    Update combine_clusters_from_multiple_probes.m

--- place_cell_analysis/combine_clusters_from_multiple_probes.m
+++ place_cell_analysis/combine_clusters_from_multiple_probes.m
@@ -3,15 +3,23 @@ function clusters_combined = combine_clusters_from_multiple_probes(clusters1,clu
 %
 all_fields = fieldnames(clusters1);
 
-
-for ncell =1:length(clusters2.cluster_id)
-    % give cluster 2 spikes new cluster id
-    if isfield(clusters1,'spike_id')
+if isfield(clusters2,'cluster_id')
+    for ncell =1:length(clusters2.cluster_id)
+        % give cluster 2 spikes new cluster id
         clusters2.spike_id(clusters2.spike_id == clusters2.cluster_id(ncell)) = clusters2.cluster_id(ncell) + 10000;
+        clusters2.cluster_id(ncell) = clusters2.cluster_id(ncell) + 10000;
+
+    end
+end
+
+if isfield(clusters2,'merged_cluster_id')
+    merged_id = unique(clusters2.merged_cluster_id);
+    for ncell =1:length(merged_id)
+        clusters2.merged_spike_id(clusters2.merged_spike_id == merged_id(ncell)) = merged_id(ncell) + 10000;
     end
 
-    if isfield(clusters1,'cluster_id')
-        clusters2.cluster_id(ncell) = clusters2.cluster_id(ncell) + 10000;
+    for ncell =1:length(clusters2.merged_cluster_id)
+        clusters2.merged_cluster_id(ncell) = clusters2.merged_cluster_id(ncell) + 10000;
     end
 end
 
@@ -34,5 +42,6 @@ if isfield(clusters1,'merged_spike_id')
     clusters_combined.merged_spike_id = clusters_combined.merged_spike_id(spike_index);
     clusters_combined.spike_times = clusters_combined.spike_times(spike_index);
 end
+
 
 end
