4d5342126f2b61739a22c93cf1d3b52bbb512fd9
masahirotaki <masahirotakigaw@gmail.com>
2024-03-15 13:46:51 +0000

    update

--- main_summary_plot.m
+++ main_summary_plot.m
@@ -191,6 +191,8 @@ for track_id = 1:2
     place_fields_all(track_id).x_bin_centres =[];
     place_fields_all(track_id).average_map = [];
     place_fields_all(track_id).raw = [];
+    place_fields_all(track_id).odd_lap_map = [];
+    place_fields_all(track_id).even_lap_map = [];
 
     place_fields_all(track_id).odd_even_corr = [];
     place_fields_all(track_id).odd_even_stability = [];
@@ -221,6 +223,10 @@ for track_id = 1:2
 
     place_fields_all(track_id).spatial_cell_correlation = [];
     place_fields_all(track_id).ripple_cell_correlation = [];
+
+    place_fields_all(track_id).SMI = [];
+    place_fields_all(track_id).lap_SMI = [];
+    place_fields_all(track_id).within_block_SMI = [];
 end
 
 place_fields_all_L = place_fields_all;
@@ -375,6 +381,10 @@ for nsession =[1 2 3 4 6 7 8 9 10 12 14]
                     place_fields_all_L(track_id).raw = [place_fields_all_L(track_id).raw;place_fields(track_id).raw(good_cell_index)];
                     place_fields_all_L(track_id).average_map = [place_fields_all_L(track_id).average_map squeeze(mean(ratemap_matrix,1))];
 
+                    % spatial modulation
+                    place_fields_all(track_id).SMI = [];
+                    place_fields_all(track_id).lap_SMI = [];
+                    place_fields_all(track_id).within_block_SMI = [];
                 end
 
             elseif options.probe_hemisphere == 2
@@ -474,6 +484,7 @@ for nsession =[1 2 3 4 6 7 8 9 10 12 14]
                     place_fields_all_R(track_id).raw = [place_fields_all_R(track_id).raw;place_fields(track_id).raw(good_cell_index)];
                     place_fields_all_R(track_id).average_map = [place_fields_all_R(track_id).average_map squeeze(mean(ratemap_matrix,1))];
 
+                    % spatial modulation
                 end
 
             end
@@ -552,77 +563,6 @@ for ncell = 1:length(cluster_id)
         counter = counter + 1;
     end
 end
-
-figure
-average_map = normalize([place_fields_all_L(1).average_map(:,selected_cells_L);place_fields_all_L(2).average_map(:,selected_cells_L)],'range')';
-average_map = reshape(average_map,size(average_map,1),size(place_fields_all_L(1).average_map,1),[]);
-subplot(2,2,1)
-imagesc(squeeze(average_map(:,:,1)));
-xlabel('position')
-title('Track Left')
-
-subplot(2,2,2)
-imagesc(squeeze(average_map(:,:,2)));
-xlabel('position')
-title('Track Right')
-
-average_map = normalize([place_fields_all_R(1).average_map(:,selected_cells_R);place_fields_all_R(2).average_map(:,selected_cells_R)],'range')';
-average_map = reshape(average_map,size(average_map,1),size(place_fields_all_R(1).average_map,1),[]);
-subplot(2,2,3)
-imagesc(squeeze(average_map(:,:,1)));
-xlabel('position')
-title('Track Left')
-
-subplot(2,2,4)
-imagesc(squeeze(average_map(:,:,2)));
-xlabel('position')
-title('Track Right')
-
-
-figure
-rippple_PSTH = normalize([place_fields_all_L(1).ripple_PSTH(:,selected_cells_L);place_fields_all_L(2).ripple_PSTH(:,selected_cells_L)],'range')';
-rippple_PSTH = reshape(rippple_PSTH,size(rippple_PSTH,1),size(place_fields_all_L(1).ripple_PSTH,1),[]);
-subplot(2,2,1)
-imagesc(squeeze(rippple_PSTH(:,:,1)));
-xline(50)
-xlabel('Time')
-title('L ripple on Track Left')
-
-subplot(2,2,2)
-imagesc(squeeze(rippple_PSTH(:,:,2)));
-xline(50)
-xlabel('position')
-title('L ripple on Track Right')
-
-rippple_PSTH = normalize([place_fields_all_R(1).ripple_PSTH(:,selected_cells_R);place_fields_all_R(2).ripple_PSTH(:,selected_cells_R)],'range')';
-rippple_PSTH = reshape(rippple_PSTH,size(rippple_PSTH,1),size(place_fields_all_R(1).ripple_PSTH,1),[]);
-subplot(2,2,3)
-imagesc(squeeze(rippple_PSTH(:,:,1)));
-xline(50)
-xlabel('Time')
-title('R ripple on Track Left')
-
-subplot(2,2,4)
-imagesc(squeeze(rippple_PSTH(:,:,2)));
-xline(50)
-xlabel('position')
-title('R ripple on Track Right')
-
-
-figure
-subplot(2,2,1)
-imagesc(normalize(reshape([place_fields_all_L(1).theta_phase_map{selected_cells_L}],size(place_fields_all_R(1).theta_phase_map{1},2),[]),'range')');
-title('L theta on Track Left')
-subplot(2,2,2)
-imagesc(normalize(reshape([place_fields_all_L(2).theta_phase_map{selected_cells_L}],size(place_fields_all_R(1).theta_phase_map{1},2),[]),'range')');
-title('L theta on Track Right')
-subplot(2,2,3)
-imagesc(normalize(reshape([place_fields_all_R(1).theta_phase_map{selected_cells_R}],size(place_fields_all_R(1).theta_phase_map{1},2),[]),'range')');
-title('R theta on Track Left')
-subplot(2,2,4)
-imagesc(normalize(reshape([place_fields_all_R(2).theta_phase_map{selected_cells_R}],size(place_fields_all_R(1).theta_phase_map{1},2),[]),'range')');
-title('R theta on Track Right')
-sgtitle('Theta')
 
 
 theta_phase_map = reshape([place_fields_all_R(1).theta_phase_map{1:2}],[],size(place_fields_all_R(1).theta_phase_map{1},2))
