e6b6a1d262e9d2c85f843b712d6918db2e5450c5
masahirotaki <masahirotakigaw@gmail.com>
2024-03-27 12:19:13 +0000

    update

--- main_ripple_analysis.m
+++ main_ripple_analysis.m
@@ -308,6 +308,14 @@ option = 'bilateral';
 experiment_info = subject_session_stimuli_mapping(SUBJECTS,option);
 Stimulus_type = 'RUN';
 % [1 2 3 4 9 10 12 14]
+load(fullfile('D:\corticohippocampal_replay\summary','place_fields_all.mat'),'place_fields_all_L','place_fields_all_R','place_fields_all_combined')
+
+psthBinSize = 0.001; %create 1ms bin
+% Define Gaussian window for smoothing
+gaussianWindow = gausswin(0.2*1/psthBinSize);
+% Normalize to have an area of 1 (i.e., to be a probability distribution)
+gaussianWindow = gaussianWindow / sum(gaussianWindow);
+
 
 for nsession = [1 2 3 4 6 7 8 9 10 12 14]
     session_info = experiment_info(nsession).session(contains(experiment_info(nsession).StimulusName,Stimulus_type));
@@ -342,7 +350,9 @@ for nsession = [1 2 3 4 6 7 8 9 10 12 14]
 
         load(fullfile(options.ANALYSIS_DATAPATH,'extracted_place_fields.mat'));
         load(fullfile(options.ANALYSIS_DATAPATH,sprintf('extracted_ripple_events%s.mat',erase(stimulus_name{n},'Masa2tracks'))));
-        load(fullfile(options.ANALYSIS_DATAPATH,'ripple_modulation.mat'),"ripple_modulation_L","ripple_modulation_R","ripple_modulation_combined")
+%         tic
+%         load(fullfile(options.ANALYSIS_DATAPATH,'ripple_modulation.mat'))
+%         toc
 
         if length(clusters) > 1
             clusters_combined = combine_clusters_from_multiple_probes(merged_clusters(1),merged_clusters(2));
@@ -354,48 +364,157 @@ for nsession = [1 2 3 4 6 7 8 9 10 12 14]
         ripple_modulation_R = [];
         ripple_modulation_combined = [];
 
-        for nprobe = 1:length(clusters)
+        metric_param = create_cluster_selection_params('sorting_option',sorting_option);
+        metric_param.unstable_ids = @(x) x==0;
+        %             metric_param.region = @(x) contains(x,'V1');
+        metric_param.merged_cluster_id = @(x) ismember(x,clusters_combined.merged_cluster_id);
+        %             metric_param.merged_cluster_id = @(x) ismember(x,clusters_combined.merged_cluster_id(ia));
+        [selected_clusters,cluster_id] = select_clusters(clusters_combined,metric_param);
+        [C,ia,ic] = unique(selected_clusters.merged_cluster_id);
 
-            options = session_info(n).probe(nprobe);
-            probe_no = session_info(n).probe(nprobe).probe_id + 1;
-            options.probe_no = probe_no; % probe_no is [1,2] it is redundant as we have options.probe_id (0 and 1)
-            [C,ia,ic] = unique(clusters_combined.merged_cluster_id);
-            
-            event_id = [ones(1,length(ripples(nprobe).T1_onset)) 2*ones(1,length(ripples(nprobe).T2_onset))];
-            [event_times,index] = sort([ripples(nprobe).T1_onset ripples(nprobe).T2_onset]);
-
-            if options.probe_hemisphere == 1
-                [ripple_modulation_L]= ripple_modulation_analysis(clusters_combined.spike_times,clusters_combined.merged_spike_id,Task_info,Behaviour,[-2 2],0.02,...
-                'unit_depth',clusters_combined.peak_depth(ia),'unit_region',clusters_combined.region(ia),'unit_id',C,'event_times',event_times',...
-                'event_label',{'Track 1','Track 2'},'event_id',event_id(index)','place_fields',place_fields);
-
-            elseif options.probe_hemisphere == 2
-                [ripple_modulation_R]= ripple_modulation_analysis(clusters_combined.spike_times,clusters_combined.merged_spike_id,Task_info,Behaviour,[-2 2],0.02,...
-                'unit_depth',clusters_combined.peak_depth(ia),'unit_region',clusters_combined.region(ia),'unit_id',C,'event_times',event_times',...
-                'event_label',{'Track 1','Track 2'},'event_id',event_id(index)','place_fields',place_fields);
-
-            end
-
-        end
-
-        if length(session_info(n).probe) <= 1
-            if session_info(n).probe.probe_hemisphere == 1
-                ripple_modulation_combined = ripple_modulation_L;
-            elseif session_info(n).probe.probe_hemisphere == 2
-                ripple_modulation_combined = ripple_modulation_R;
-            end
+        event_id = [ones(1,length(ripples(1).T1_onset)) ones(1,length(ripples(2).T1_onset)) 2*ones(1,length(ripples(1).T2_onset)) 2*ones(1,length(ripples(2).T2_onset))];
+        
+        if length(merged_clusters)>1
+            probe_hemisphere = [session_info(n).probe.probe_hemisphere];
+            temp = [ones(1,length(ripples(1).T1_onset)) 2*ones(1,length(ripples(2).T1_onset)) ones(1,length(ripples(1).T2_onset)) 2*ones(1,length(ripples(2).T2_onset))];
         else
-% 
-%             event_times = sort([ripples(1).onset ripples(2).onset]);
-            [C,ia,ic] = unique(clusters_combined.merged_cluster_id);
-
-            event_id = [ones(1,length(ripples(1).T1_onset)) ones(1,length(ripples(2).T1_onset)) 2*ones(1,length(ripples(1).T2_onset)) 2*ones(1,length(ripples(2).T2_onset))];
-            [event_times,index] = sort([ripples(1).T1_onset ripples(2).T1_onset ripples(1).T2_onset ripples(2).T2_onset]);
-
-            [ripple_modulation_combined]= ripple_modulation_analysis(clusters_combined.spike_times,clusters_combined.merged_spike_id,Task_info,Behaviour,[-2 2],0.02,...
-                'unit_depth',clusters_combined.peak_depth(ia),'unit_region',clusters_combined.region(ia),'unit_id',C,'event_times',event_times',...
-                'event_label',{'Track 1','Track 2'},'event_id',event_id(index)','place_fields',place_fields);
+            probe_hemisphere = [session_info(n).probe.probe_hemisphere];
+            temp = ones(1,length(event_id));
         end
+
+        % whole session timevec variables
+        timevec = Behaviour.tvec';
+        timevec_edge = (timevec(1)-(psthBinSize)/2....
+            :psthBinSize:...
+            timevec(end)+(psthBinSize)/2)';
+
+        probe_id = temp;
+        for nprobe = 1:length(merged_clusters)
+            probe_id(temp==nprobe) = probe_hemisphere(nprobe);
+        end
+
+        [event_times,index] = sort([ripples(1).T1_onset ripples(2).T1_onset ripples(1).T2_onset ripples(2).T2_onset]);
+        event_id = event_id(index);
+        probe_id = probe_id(index);
+
+        all_spike_counts = [];
+        %         event_id = [ones(1,size(ripple_modulation_combined(1).spike_count{ncell},1))  2*ones(1,size(ripple_modulation_combined(2).spike_count{ncell},1))];
+        %         ripple_cells = unique([find(ripple_modulation_combined(1).ripple_modulation_percentile>=0.95) find(ripple_modulation_combined(2).ripple_modulation_percentile>=0.95)]);
+        ripple_cells = 1:length(ripple_modulation_combined(1).ripple_modulation_percentile);
+
+        for ncell = 1:length(ripple_cells)
+
+            spike_times = clusters_combined.spike_times(clusters_combined.merged_spike_id == ripple_modulation_combined(1).cluster_id(ripple_cells(ncell)));
+
+            %             % Convolve spike count time series with Gaussian window
+            %             y = histcounts(spike_times, timevec_edge)';
+            %             y = conv(y, gaussianWindow, 'same')/psthBinSize; % all spikes
+
+            spike_speed = interp1(Behaviour.tvec,Behaviour.speed,spike_times,'nearest');
+            spike_times = spike_times(spike_speed<5);
+
+            [psth, bins, rasterX, rasterY, spikeCounts, binnedArray] = psthAndBA(spike_times,...
+                event_times, [-2 2], psthBinSize);
+            all_spike_counts(ncell,:,:) = binnedArray(:,bins<0.5&bins>-0.5)'; % ncell X time x events
+
+%             if size(binnedArray,1)==1
+%                 psth = conv(binnedArray/psthBinSize,gaussianWindow,'same');
+%             else
+%                 for nevent = 1:size(binnedArray,1)
+%                     psth(nevent,:) = conv(binnedArray(nevent,:)/psthBinSize,gaussianWindow,'same');
+%                 end
+                %                 psth = (psth-mean(y))/std(y);
+                %                 psth_track1 = mean(psth_track1,'omitnan');
+%             end
+
+%             all_spike_counts(ncell,:,:) = psth(:,bins<0.5&bins>-0.5)'; % ncell X time x events
+            %                 all_spike_counts{track_id}(ncell,:,:) = [ripple_modulation_combined(1).spike_count{ncell}(:,bins<0.5&bins>-0.5);...
+            %                     ripple_modulation_combined(2).spike_count{ncell}(:,bins<0.5&bins>-0.5);]; % ncell X time x events
+        end
+
+        % V1 L and R
+        ripple_spike_count = [];
+        cell_regions = ripple_modulation_combined(1).region(ripple_cells);
+        for nprobe = 1:length(probe_hemisphere)
+
+            if session_info(n).probe(nprobe).probe_hemisphere == 1
+                ripple_spike_count{1} = all_spike_counts(contains(cell_regions,'V1_L'),:,:);
+            elseif session_info(n).probe(nprobe).probe_hemisphere == 2
+                ripple_spike_count{2} = all_spike_counts(contains(cell_regions,'V1_R'),:,:);
+            end
+
+        end
+
+        % HPC
+        ripple_spike_count{3} = all_spike_counts(contains(cell_regions,'HPC'),:,:);
+
+
+        %%% Example computation of a cross-correlation map
+        % Figs. 3 and 4
+
+        % The units of the arguments are with respect to the binning window used
+        % to bin spikes.
+        argIn=[];
+        argIn.BinWidth = 1;     % 10ms
+        argIn.MaxDelay = 50;    % 500ms
+        argIn.TimeStep = 4;     % 40ms
+        argIn.WindowLength = 8; % 80ms
+
+        %argIn.NumWorkers = Inf; % Requires Parallel Processing Toolbox
+
+        disp(argIn)
+        argOut = ComputeCorrMap({ripple_spike_count{3} ripple_spike_count{1}}, event_id, argIn);
+
+        %%%
+        CANONICAL_PAIR_IDX = 1;
+        mapDim = size(argOut.CorrMap, 2);
+        delays = (-argIn.MaxDelay:argIn.MaxDelay)*10; % Convert to ms
+        t = (-0.5*100:argIn.TimeStep:0.5*100)*10; % Convert to ms
+
+        figure(1);
+
+        imagesc( delays, t, argOut.CorrMap(:,:,CANONICAL_PAIR_IDX)' )
+
+        ax = gca;
+        ax.YDir = 'Normal';
+
+        xlabel('Delay')
+        ylabel('Time')
+        colorbar
+        clim([0.1 0.4])
+        colormap(flip(gray))
+
+        %%% Example computation of the interaction structure analysis
+        % Fig. 6
+
+        clear argIn
+
+        argIn.TimePeriods = [...
+            (  0:20:40)' ( 20:20:60)'; ...
+            (128:20:168)' (148:20:188)'] + 5;
+
+        % Can take up to 15min due to the 10-fold cross-validation
+        argOut = CovStabilityAcrossTimeAnalysis(spikes, expCond, argIn);
+
+        %%%
+        RANK_TO_PLOT = 2;
+
+        normFactor = diag(argOut.CvR(:,:,RANK_TO_PLOT));
+        numTimePeriods = size(argIn.TimePeriods, 1);
+
+        figure(2);
+
+        imagesc(argOut.CvR(:,:,RANK_TO_PLOT)./repmat(normFactor', numTimePeriods, 1))
+
+        ax = gca;
+        ax.YDir = 'Normal';
+
+        axis square
+
+        xlabel('Time Used For Correlation')
+        ylabel('Time Used For Fitting')
+
+
         save(fullfile(options.ANALYSIS_DATAPATH,'ripple_modulation.mat'),"ripple_modulation_L","ripple_modulation_R","ripple_modulation_combined")
 
     end
