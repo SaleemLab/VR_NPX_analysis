5ab94615e18f28cfb9714528c969ac3829d880fb
masahirotaki <masahirotakigaw@gmail.com>
2024-07-10 02:34:24 +0100

    Update main_LFP_preprocessing_and_saving.m

--- main_LFP_preprocessing_and_saving.m
+++ main_LFP_preprocessing_and_saving.m
@@ -8,6 +8,22 @@ SESSION = '20240706/20240706_0';
 
 EPHYS_DATAPATH= fullfile('Z:\ibn-vision\DATA\SUBJECTS',SUBJECT,'ephys',SESSION);
 cd(fullfile('Z:\ibn-vision\DATA\SUBJECTS',SUBJECT,'ephys',SESSION))
+DIR = dir('*_g*');
+
+for nfolder = 1:length(DIR)
+    options= [];
+    temp_DIR = dir(fullfile(DIR(nfolder).folder,DIR(nfolder).name,[DIR(nfolder).name,'_imec0']));
+    if ~isempty(temp_DIR)
+        options.EPHYS_DATAPATH = fullfile(DIR(nfolder).folder,DIR(nfolder).name,[DIR(nfolder).name,'_imec0']);
+        preprocess_and_save_LFP(options)
+    end
+
+    temp_DIR = dir(fullfile(DIR(nfolder).folder,DIR(nfolder).name,[DIR(nfolder).name,'_imec1']));
+    if ~isempty(temp_DIR)
+        options.EPHYS_DATAPATH = fullfile(DIR(nfolder).folder,DIR(nfolder).name,[DIR(nfolder).name,'_imec1']);
+        preprocess_and_save_LFP(options)
+    end
+end
 
 
 all_DIR= dir(fullfile('Z:\ibn-vision\DATA\SUBJECTS',SUBJECT,'ephys','20*'));
@@ -33,7 +49,7 @@ for ndate = 1:length(all_DIR)
                 preprocess_and_save_LFP(options)
             end
 
-            cd(fullfile('Z:\ibn-vision\DATA\SUBJECTS',SUBJECT,'ephys',SESSION))
+
         end
     end
 end
