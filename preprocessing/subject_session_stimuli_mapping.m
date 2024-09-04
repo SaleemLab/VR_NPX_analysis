f301c4d0c7f31a6f60d69be5fad81e9b3c63cc61
masahirotaki <masahirotakigaw@gmail.com>
2024-09-04 14:53:00 +0100

    Update subject_session_stimuli_mapping.m

--- preprocessing/subject_session_stimuli_mapping.m
+++ preprocessing/subject_session_stimuli_mapping.m
@@ -2,12 +2,12 @@ function experiment_info = subject_session_stimuli_mapping(SUBJECTS,options)
 % Function used to create basic information about each recording sessions
 % recorded from the animals specified
 
-% 05/01/2024 M.T updating experiment info to include open field data 
-% (Currently open field ephys data is saved as a separate folder 
+% 05/01/2024 M.T updating experiment info to include open field data
+% (Currently open field ephys data is saved as a separate folder
 % (e.g. 20231215_OpenField) to avoid conflict with 20231215)
 % We need to find a good way to save multiple ephys
 % folders for the same day. Since spikeglx can not just concat all data
-% from multiple sessions, currently, we need to process each folder separately... 
+% from multiple sessions, currently, we need to process each folder separately...
 % Maybe with spikeinterface it can be sovled?
 
 
@@ -16,7 +16,7 @@ ROOTPATH = 'Z:\ibn-vision';
 % if contains(options,'bilateral')
 %     EXPERIMENT_INFO_PATH = 'Z:\ibn-vision\USERS\Masa\recording_info';
 % else
-%     
+%
 % end
 
 
@@ -27,15 +27,15 @@ nexperiment = 1;
 
 for nsubject = 1:length(SUBJECTS)
     if contains(options,'bilateral')
-        
-%         stimuli_info = readtable('C:\Users\masah\Documents\GitHub\VR_NPX_analysis\session_stimuli_table','Sheet',SUBJECTS{nsubject});
-stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimuli_table','Sheet',SUBJECTS{nsubject});
+
+        %         stimuli_info = readtable('C:\Users\masah\Documents\GitHub\VR_NPX_analysis\session_stimuli_table','Sheet',SUBJECTS{nsubject});
+        stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimuli_table','Sheet',SUBJECTS{nsubject});
         % stimuli_info = readtable('C:\Users\masah\Documents\GitHub\VR_NPX_analysis\session_stimuli_table','Sheet',SUBJECTS{nsubject});
-        % dataCell = readcell('Z:\ibn-vision\USERS\Masa\recording_info\session_stimuli_table','Sheet',SUBJECTS{nsubject}); % this can create missing object when session id is missing.
-        dataCell = readcell('C:\Users\masah\Documents\GitHub\VR_NPX_analysis\session_stimuli_table','Sheet',SUBJECTS{nsubject});
+        dataCell = readcell('Z:\ibn-vision\USERS\Masa\recording_info\session_stimuli_table','Sheet',SUBJECTS{nsubject}); % this can create missing object when session id is missing.
+%         dataCell = readcell('C:\Users\masah\Documents\GitHub\VR_NPX_analysis\session_stimuli_table','Sheet',SUBJECTS{nsubject});
         % Convert the cell array to a table
         stimuli_info_table = cell2table(dataCell(2:end,:),"VariableNames",stimuli_info.Properties.VariableNames); % not the best way but works
-        
+
     elseif contains(options,'V1-MEC')
         stimuli_info = readtable(['Z:\ibn-vision\USERS\Diao\ephys_recording_info\session_stimuli_table_DT'],'Sheet',SUBJECTS{nsubject});
         dataCell = readcell('Z:\ibn-vision\USERS\Diao\ephys_recording_info\session_stimuli_table_DT','Sheet',SUBJECTS{nsubject});
@@ -49,7 +49,7 @@ stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimul
     clear stimuli_info_table
 
     all_dates_this_animal = unique(stimuli_info.date);
-   
+
     if contains('session_id',stimuli_info.Properties.VariableNames) % if session number is specifeid (since chronic recording)
         all_sessions_this_animal = stimuli_info.session_id; % this only works if there is at least something in session id column
 
@@ -106,17 +106,17 @@ stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimul
             stimuli_info.MEC = cellstr(num2str(stimuli_info.MEC));
         end
     end
-        
+
     for nsession = 1:length(all_dates_this_animal)
-        
+
         experiment_info(nexperiment).experiment_ID = [SUBJECTS{nsubject},'_',num2str(all_dates_this_animal(nsession))];
         experiment_info(nexperiment).date = all_dates_this_animal(nsession);
         experiment_info(nexperiment).subject = SUBJECTS{nsubject};
 
         DATAPATH = fullfile(ROOTPATH,'DATA','SUBJECTS');
 
-%         EPHYS_DATAPATH = fullfile(DATAPATH,SUBJECTS{nsubject},'ephys',num2str(all_sessions_this_animal(nsession)));
-%         EPHYS_DATAPATH_OpenField = fullfile(DATAPATH,SUBJECTS{nsubject},'ephys',[num2str(all_sessions_this_animal(nsession)),'_OpenField']);
+        %         EPHYS_DATAPATH = fullfile(DATAPATH,SUBJECTS{nsubject},'ephys',num2str(all_sessions_this_animal(nsession)));
+        %         EPHYS_DATAPATH_OpenField = fullfile(DATAPATH,SUBJECTS{nsubject},'ephys',[num2str(all_sessions_this_animal(nsession)),'_OpenField']);
 
         experiment_info(nexperiment).ANALYSIS_DATAPATH = fullfile(DATAPATH,SUBJECTS{nsubject},'analysis',num2str(all_dates_this_animal(nsession)));
         experiment_info(nexperiment).BONSAI_DATAPATH = fullfile(DATAPATH,SUBJECTS{nsubject},'stimuli',num2str(all_dates_this_animal(nsession)));
@@ -137,7 +137,7 @@ stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimul
             experiment_info(nexperiment).probe_V1 = stimuli_info.V1(stimuli_info.date == all_dates_this_animal(nsession));
             experiment_info(nexperiment).probe_MEC = stimuli_info.MEC(stimuli_info.date == all_dates_this_animal(nsession));
         end
-        
+
         for nstimuli = 1:length(StimulusName)
 
             % Bonsai data path
@@ -151,14 +151,14 @@ stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimul
                     bonsai_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*',this_stimulus, '_*.csv']));
                 elseif contains(this_stimulus,'Masa2tracks')
                     bonsai_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*','Masa2tracks', '*.csv']));
-%                 elseif contains(this_stimulus,'Sleep')
-%                     bonsai_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*','Sleep', '*.csv']));
+                    %                 elseif contains(this_stimulus,'Sleep')
+                    %                     bonsai_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*','Sleep', '*.csv']));
                 else
                     bonsai_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*',this_stimulus, '*.csv']));
                 end
             end
 
-%             [~,idx] = sort([bonsai_files.datenum]);
+            %             [~,idx] = sort([bonsai_files.datenum]);
 
             file_time = extractPattern(bonsai_files(stimulus_counter(nstimuli)).name); % sometimes the exact seconds can be off for different bonsai files, so search is currently based on hour and minute (may need optimisation);
             bonsai_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*',file_time,'*.csv']));
@@ -169,7 +169,7 @@ stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimul
                 % Unfortunately... sometimes bin file time stamp can be 1 or 2
                 % mins later than others....
                 bin_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*',this_stimulus,'*.bin']));
-%                 [~,idx] = sort([bin_files.datenum]);
+                %                 [~,idx] = sort([bin_files.datenum]);
 
                 file_time1 = extractPattern(bin_files(stimulus_counter(nstimuli)).name); % sometimes the exact seconds can be off for different bonsai files, so search is currently based on hour and minute (may need optimisation);
                 bin_files = dir(fullfile(experiment_info(nexperiment).BONSAI_DATAPATH,['*',file_time1,'*.bin']));
@@ -262,7 +262,7 @@ stimuli_info = readtable('Z:\ibn-vision\USERS\Masa\recording_info\session_stimul
 
                 experiment_info(nexperiment).session(nstimuli).probe(nprobe).BONSAI_DATAPATH = experiment_info(nexperiment).BONSAI_DATAPATH;
                 experiment_info(nexperiment).session(nstimuli).probe(nprobe).bonsai_files_names = {bonsai_files.name};
-                
+
                 if ~isnan(all_trial_types_this_date{nstimuli}) % if trial type is specified (like familar or novel etc)
                     experiment_info(nexperiment).session(nstimuli).probe(nprobe).trial_type = all_trial_types_this_date{nstimuli};
                 end
@@ -279,50 +279,50 @@ end
 
 
 function [output,nameCountMap] = assignNumbersBasedOnCount(names)
-    % Create a map to store counts
-    nameCountMap = containers.Map;
+% Create a map to store counts
+nameCountMap = containers.Map;
 
-    % Initialize output array
-    output = zeros(size(names));
+% Initialize output array
+output = zeros(size(names));
 
-    % Iterate through each stimulus
-    for i = 1:length(names)
-        % Get the current name
+% Iterate through each stimulus
+for i = 1:length(names)
+    % Get the current name
 
-        currentName = names{i};
-        if contains(currentName,'Masa2tracks')% if it is Masa2tracks
-            currentName = 'Masa2tracks';
-%         elseif contains(currentName,'SleepChronic')
-%             currentName = 'SleepChronic';
-        end
-
-        % Check if the stimulus is already in the map
-        if isKey(nameCountMap, currentName)
-            % Increment the count if the stimulus is present
-            count = nameCountMap(currentName);
-            nameCountMap(currentName) = count + 1;
-        else
-            % Add the stimulus to the map if it's not present
-            nameCountMap(currentName) = 1;
-        end
-
-        % Assign the count to the output array
-        output(i) = nameCountMap(currentName);
+    currentName = names{i};
+    if contains(currentName,'Masa2tracks')% if it is Masa2tracks
+        currentName = 'Masa2tracks';
+        %         elseif contains(currentName,'SleepChronic')
+        %             currentName = 'SleepChronic';
     end
+
+    % Check if the stimulus is already in the map
+    if isKey(nameCountMap, currentName)
+        % Increment the count if the stimulus is present
+        count = nameCountMap(currentName);
+        nameCountMap(currentName) = count + 1;
+    else
+        % Add the stimulus to the map if it's not present
+        nameCountMap(currentName) = 1;
+    end
+
+    % Assign the count to the output array
+    output(i) = nameCountMap(currentName);
+end
 end
 
 
 function extractedPattern = extractPattern(filename)
-    % Define the regular expression pattern
-    pattern = '(\d{4}-\d{2}-\d{2}T\d{2}_\d{2})';
+% Define the regular expression pattern
+pattern = '(\d{4}-\d{2}-\d{2}T\d{2}_\d{2})';
 
-    % Find the match in the filename using regular expression
-    match = regexp(filename, pattern, 'match');
+% Find the match in the filename using regular expression
+match = regexp(filename, pattern, 'match');
 
-    % Check if a match is found
-    if ~isempty(match)
-        extractedPattern = match{1};
-    else
-        extractedPattern = 'No match found';
-    end
+% Check if a match is found
+if ~isempty(match)
+    extractedPattern = match{1};
+else
+    extractedPattern = 'No match found';
+end
 end
