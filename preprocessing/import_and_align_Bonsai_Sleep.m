f6af38a894714fcf5fe86cdd788a0279cf2773b7
masahirotaki <masahirotakigaw@gmail.com>
2024-09-30 22:21:20 +0100

    update sync xcross and interp1 for aligning signals

--- preprocessing/import_and_align_Bonsai_Sleep.m
+++ preprocessing/import_and_align_Bonsai_Sleep.m
@@ -24,7 +24,8 @@ Nidq = [];
 td = dir(fullfile(options(1).EPHYS_DATAPATH,'..','*NidqTimes*'));
 if ~isempty(td)
     load(fullfile(options(1).EPHYS_DATAPATH,'..',td(1).name),'Nidq');
-    syncTimes_ephys = Nidq.bonsai_sync_on;% use upswings currently
+    syncTimes_ephys_on = Nidq.bonsai_sync_on;% use upswings currently
+    syncTimes_ephys_off = Nidq.bonsai_sync_off;% use upswings currently
 else
     error('nidq and ephys sync pulse extraction and alignment not done!')
     return
@@ -49,10 +50,23 @@ Sync_pulse = table2array(this_table(:,4)); %
 
 % photodiode_smoothed = smoothdata(table2array(this_table(:,4)),'movmedian',5); %
 peripherals = [];
+peripherals.Sync_pulse = Sync_pulse;
 peripherals.Sync = kmeans(Sync_pulse-movmean(Sync_pulse,120),2)-1;
+if mean(peripherals.Sync_pulse(peripherals.Sync==1))<mean(peripherals.Sync_pulse(peripherals.Sync==0))
+    % if 0 is when Sync goes up, swap 1 and 0
+    temp = peripherals.Sync;
+    peripherals.Sync(temp==0)=1;
+    peripherals.Sync(temp==1)=0;
+end
+
 peripherals.Time =  behaviour.computer_time_original*1000;
 idx_trial_start = find(diff(peripherals.Sync)==1) + 1;
 idx_trial_end = find(diff(peripherals.Sync)==-1) + 1;
+
+% figure;
+% plot(mean(Sync_pulse)/3+mean(Sync_pulse)/2*kmeans(Sync_pulse-movmean(Sync_pulse,120),2)-1)
+% hold on;
+% plot(Sync_pulse)
 
 if idx_trial_end(1)<idx_trial_start(1)
     idx_trial_end(1)= [];
@@ -94,24 +108,50 @@ if length(Nidq.bonsai_sync_on) == length(Nidq.bonsai_sync_off)+1
     Nidq.bonsai_sync_on(end)=[];
 end
 
-% [peripherals] = alignBonsaiToNidqSyncTimes(peripherals,sort([Nidq.bonsai_sync_on Nidq.bonsai_sync_off])); % use upswings currently
-[peripherals] = alignBonsaiToEphysSyncTimes(peripherals,Nidq.bonsai_sync_on); % use upswings currently
-behaviour.sglxTime_original = peripherals.sglxTime;
 
-behaviour.sglxTime = interp1(behaviour.sglxTime_original,behaviour.sglxTime_original,behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
-            
+[peripherals] = alignBonsaiToEphysSyncTimes(peripherals,Nidq.bonsai_sync_on); % use upswings currently
+
+temp = peripherals;
+temp.Time= peripherals.sglxTime*1000;
+temp.Sync(peripherals.Sync==0)=1;
+temp.Sync(peripherals.Sync==1)=0;
+[peripherals] = alignBonsaiToEphysSyncTimes(temp,Nidq.bonsai_sync_off); % use upswings currently
+
+[unique_time,ia,~]=unique(peripherals.sglxTime);
+
+
+behaviour.sglxTime_original = unique_time;
+
+
+behaviour.sglxTime = interp1(behaviour.sglxTime_original,behaviour.sglxTime_original,behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');            
 behaviour.tvec = behaviour.sglxTime;
 
-behaviour.mobility = interp1(behaviour.sglxTime_original,behaviour.mobility_original,behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
-behaviour.X = interp1(behaviour.sglxTime_original,behaviour.mobility_original,behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
-behaviour.Y = interp1(behaviour.sglxTime_original,behaviour.mobility_original,behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
-behaviour.Sync_pulse = interp1(behaviour.sglxTime_original,Sync_pulse',behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
+behaviour.mobility = interp1(behaviour.sglxTime_original,behaviour.mobility_original(ia),behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
+behaviour.X = interp1(behaviour.sglxTime_original,behaviour.X_original(ia),behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
+behaviour.Y = interp1(behaviour.sglxTime_original,behaviour.Y_original(ia),behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
+behaviour.Sync_pulse = interp1(behaviour.sglxTime_original,Sync_pulse(ia)',behaviour.sglxTime_original(1):1/60:behaviour.sglxTime_original(end),'linear');
 
 % tvec = resample(Behaviour.tvec,Behaviour.tvec,60);
+% figure
+% plot(peripherals.sglxTime(1:300000),peripherals.Sync(1:300000)*2000+10000);
+% hold on;plot(Nidq.sglxTime(1:3000000),Nidq.bonsai_sync(1:3000000));...
+%     hold on;scatter(Nidq.bonsai_sync_on(1:50),20000*ones(1,50));
+% hold on;scatter(Nidq.bonsai_sync_off(1:50),10000*ones(1,50));
+% 
+% figure
+% plot(peripherals.sglxTime,peripherals.Sync*2000+10000);
+% hold on;plot(Nidq.sglxTime,Nidq.bonsai_sync);...
+%     hold on;scatter(Nidq.bonsai_sync_on(1:500),20000*ones(1,500));
+% hold on;scatter(Nidq.bonsai_sync_off(1:500),10000*ones(1,500));
 
-% plot(peripherals.sglxTime(1:300000),peripherals.Sync(1:300000)*2000+10000);hold on;plot(Nidq.sglxTime(1:3000000),Nidq.bonsai_sync(1:3000000))
+% figure
+% plot(Nidq.sglxTime(1:3000000),Nidq.bonsai_sync(1:3000000));hold on;
+% plot(peripherals.sglxTime(1:300000),Sync_pulse(1:300000)./10);
+% plot(peripherals.sglxTime(1:300000),peripherals.Sync(1:300000)*2000+10000);
+% plot(behaviour.sglxTime_original(1:300000),Sync_pulse(1:300000)./10);
+% 
 % plot(behaviour.sglxTime(1:300000),behaviour.Sync_pulse(1:300000)-mean(behaviour.Sync_pulse));hold on;plot(Nidq.sglxTime(1:3000000),Nidq.bonsai_sync(1:3000000)-mean(Nidq.bonsai_sync))
 
 
-
+% plot(peripherals.Time(1:300000)./1000-peripherals.Time(1),peripherals.Sync(1:300000)*2000+10000);hold on;plot(Nidq.sglxTime(1:3000000),Nidq.bonsai_sync(1:3000000))
 end
