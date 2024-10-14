7eebea270cf0ce24b61d737071f7ad10446fec48
masahirotaki <masahirotakigaw@gmail.com>
2024-10-14 13:32:18 +0100

    Update main_ripple_analysis.m

--- main_ripple_analysis.m
+++ main_ripple_analysis.m
@@ -12,7 +12,7 @@ SUBJECTS={'M24016','M24017','M24018'};
 option = 'bilateral';
 experiment_info = subject_session_stimuli_mapping(SUBJECTS,option);
 experiment_info = subject_session_stimuli_mapping(SUBJECTS,option);
-experiment_info=experiment_info([6 9 14 21 22 27 35 38 40]);
+experiment_info=experiment_info([6 9 14 19 21 22 27 35 38 40]);
 Stimulus_type = 'Sleep';
 % Stimulus_type = 'RUN';
 % [1 2 3 4 9 10 12 14]
