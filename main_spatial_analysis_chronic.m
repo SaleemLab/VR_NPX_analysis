d1e8402312c624c1f776de62e0fd4fad8d89816c
masahirotaki <masahirotakigaw@gmail.com>
2024-10-15 11:20:00 +0100

    Update main_spatial_analysis_chronic.m

--- main_spatial_analysis_chronic.m
+++ main_spatial_analysis_chronic.m
@@ -10,7 +10,7 @@ clear all
 SUBJECTS={'M24016','M24017','M24018'};
 option = 'bilateral';
 experiment_info = subject_session_stimuli_mapping(SUBJECTS,option);
-experiment_info=experiment_info([6 9 14 21 22 27 35 38 40]);
+experiment_info=experiment_info([6 9 14 19 21 22 27 35 38 40]);
 Stimulus_type = 'RUN1';
 
 for nsession = 1:length(experiment_info)
