94cc36c0f4db9c99985bff76c53b37217c794b95
masahirotaki <masahirotakigaw@gmail.com>
2024-10-19 16:21:43 +0100

    update

--- main_reactivation_analysis.m
+++ main_reactivation_analysis.m
@@ -24,7 +24,7 @@ Stimulus_type = 'Sleep';
 % Stimulus_types_all = {'RUN','POST'};
 
 
-for nsession = [4 10]
+for nsession = 1:length(experiment_info)
     session_info = experiment_info(nsession).session(contains(experiment_info(nsession).StimulusName,Stimulus_type));
     stimulus_name = experiment_info(nsession).StimulusName(contains(experiment_info(nsession).StimulusName,Stimulus_type));
     load(fullfile(session_info(1).probe(1).ANALYSIS_DATAPATH,'..','best_channels.mat'));
