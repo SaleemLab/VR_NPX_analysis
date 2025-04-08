df3d0951d3ba970d403181037c2b92e224e2e8a2
Di'ao Tong <61952184+atombysx@users.noreply.github.com>
2025-04-08 15:08:24 +0100

    1

--- main_LFP_preprocessing_and_saving.m
+++ main_LFP_preprocessing_and_saving.m
@@ -7,9 +7,13 @@ addpath(genpath('C:\Users\adam.tong\Documents\GitHub\VR_NPX_analysis'))
 
 %% LFP preprocess -> Catgt
 
+<<<<<<< Updated upstream
 
 SUBJECT = 'M25002';
 
+=======
+SUBJECT = 'M25002';
+>>>>>>> Stashed changes
 all_DIR= dir(fullfile('Z:\ibn-vision\DATA\SUBJECTS',SUBJECT,'ephys','20*'));
 
 for ndate = 2
