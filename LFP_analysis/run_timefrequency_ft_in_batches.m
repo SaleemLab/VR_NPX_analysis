ad120b7123eb7599d2347ad51321de9e2082ca34
masahirotaki <masahirotakigaw@gmail.com>
2025-06-28 22:18:14 +0100

    update batch process

---
+++ LFP_analysis/run_timefrequency_ft_in_batches.m
@@ -0,0 +1,45 @@
+function [FFT,timebin,freqs] = run_timefrequency_ft_in_batches(cfg, data, batch_size)
+% Batch process trials in FieldTrip frequency analysis
+%
+% cfg        - your FieldTrip config (including all mtmconvol options)
+% data       - FieldTrip data structure (with .trial, .time, etc.)
+% batch_size - number of trials per batch (e.g., 500)
+
+nTrials = numel(data.trial);
+batch_starts = 1:batch_size:nTrials;
+
+TFR_batch = [];
+FFT = [];  % initialize output
+
+for i = 1:length(batch_starts)
+    idx_start = batch_starts(i);
+    idx_end   = min(idx_start + batch_size - 1, nTrials);
+
+    fprintf('Processing trials %d to %d...\n', idx_start, idx_end);
+
+    % Extract batch
+    batch_data = data;
+    batch_data.trial = data.trial(idx_start:idx_end);
+    batch_data.time  = data.time(idx_start:idx_end);
+
+    clear TFR_batch
+    % Run frequency analysis
+    TFR_batch = ft_freqanalysis(cfg, batch_data);
+
+    FFT_batch = permute(TFR_batch.fourierspctrm(:, :,:,TFR_batch.time <= 2 &   TFR_batch.time >= -2), [2 3 4 1]); % grab and save only 2 second windows
+    
+    if isempty(FFT)
+        FFT = FFT_batch;
+    else
+        FFT = cat(4, FFT, FFT_batch);
+    end
+end
+
+timebin = TFR_batch.time;
+freqs = TFR_batch.freq;
+
+fprintf('Done. All batches processed and concatenated.\n');
+end
+
+
+
