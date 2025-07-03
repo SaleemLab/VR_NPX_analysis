c5e5481a636bee8b511aa09889f4c6ada2a1e210
masahirotaki <masahirotakigaw@gmail.com>
2025-07-03 23:58:05 +0100

    manage warning for fieldtrip

--- LFP_analysis/run_timefrequency_ft_in_batches.m
+++ LFP_analysis/run_timefrequency_ft_in_batches.m
@@ -10,6 +10,10 @@ batch_starts = 1:batch_size:nTrials;
 
 TFR_batch = [];
 FFT = [];  % initialize output
+global ft_default
+ft_default.warning = 'no';   % turns off ft_warning
+ft_default.feedback = 'no';  % suppresses ft_progress and others
+warning('off', 'all')
 
 for i = 1:length(batch_starts)
     idx_start = batch_starts(i);
@@ -24,7 +28,9 @@ for i = 1:length(batch_starts)
 
     clear TFR_batch
     % Run frequency analysis
+    % warning('off', 'all');  % turn off all warnings
     TFR_batch = ft_freqanalysis(cfg, batch_data);
+    % warning('on', 'all');  % turn off all warnings
 
     FFT_batch = single(permute(TFR_batch.fourierspctrm(:, :,:,TFR_batch.time <= 2 &   TFR_batch.time >= -2), [2 3 4 1])); % grab and save only 2 second windows
     
@@ -37,7 +43,7 @@ end
 % FFT = single(FFT);
 timebin = TFR_batch.time;
 freqs = TFR_batch.freq;
-
+warning('on', 'all')
 fprintf('Done. All batches processed and concatenated.\n');
 end
 
